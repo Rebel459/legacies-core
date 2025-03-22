ef2ce619bee2f58635deb13c7fe85673be0cbbe7
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-03-22 20:50:21 +1100

    Update Legacies.java

--- src/main/java/net/legacy/legacies/Legacies.java
+++ src/main/java/net/legacy/legacies/Legacies.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
+import net.legacy.legacies.integration.BiolithIntegration;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
@@ -22,6 +23,8 @@ public class Legacies implements ModInitializer {
 					ResourcePackActivationType.ALWAYS_ENABLED
 			);
 		}
+
+		BiolithIntegration.INSTANCE.init();
 	}
 
 	public static ResourceLocation id(String path) {
