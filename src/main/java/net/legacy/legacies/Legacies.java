2e275eb1c1121bff2ef2b6eb693f81260c37f27e
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-06-18 23:27:29 +1000

    v1.2.7

--- src/main/java/net/legacy/legacies/Legacies.java
+++ src/main/java/net/legacy/legacies/Legacies.java
@@ -1,14 +1,11 @@
 package net.legacy.legacies;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.legacy.legacies.integration.MoreFoodLootTables;
 import net.legacy.legacies.registry.LegaciesCreativeInventorySorting;
 import net.legacy.legacies.registry.LegaciesItems;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.Optional;
@@ -23,14 +20,6 @@ public class Legacies implements ModInitializer {
 		LegaciesCreativeInventorySorting.init();
 
 		MoreFoodLootTables.modifyLootTables();
-
-		if (FabricLoader.getInstance().isModLoaded("hxp")) {
-			ResourceManagerHelper.registerBuiltinResourcePack(
-					ResourceLocation.fromNamespaceAndPath(Legacies.MOD_ID,"legacies_hxp_integration"), modContainer.get(),
-					Component.translatable("pack.legacies.hxp_integration"),
-					ResourcePackActivationType.ALWAYS_ENABLED
-			);
-		}
 	}
 
 	public static ResourceLocation id(String path) {
